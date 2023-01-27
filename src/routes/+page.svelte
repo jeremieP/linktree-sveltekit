d0fb7bc2e30fecd9703fa336e53191cdaf55e819
jeremieP <jeremie.pereira@gmail.com>
2023-01-27 13:35:51 +0100

    feat: display Social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+  import data from "$lib/data/data.json"
+  import SocialIcon from "../lib/components/SocialIcon.svelte";
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+</script>
+
+<img 
+  src="{data.me.avatar}"
+  class="rounded-full"
+  width="120"
+  height="120"
+  alt="{data.me.name}"
+/>
+<h1 class="font-bold mt-4 mb-1 text-xl text-center text-white">
+  {data.me.name}
+</h1>
+<h2 class="font-bold mb-8 text-xl text-center text-white">
+  {data.me.job.title} - <a class="underline hover:no-underline" href={data.me.job.url} target="_blank" rel="noopener noreferrer">{data.me.job.company}</a>
+</h2>
+
+
+<div class="flex items-center gap-4 mt-8 text-white">
+  {#each data.socials as item}
+    <SocialIcon {...item} />
+  {/each}
+</div>
