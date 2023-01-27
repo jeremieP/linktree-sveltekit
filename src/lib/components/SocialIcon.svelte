d27aa6d2f0ddc69643916c022e4021eac5346b0f
jeremieP <jeremie.pereira@gmail.com>
2023-01-27 13:35:29 +0100

    feat: add Socials components

---
+++ src/lib/components/SocialIcon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Github from "./icons/Github.svelte"
+  import Linkedin from "./icons/Linkedin.svelte"
+  import Twitter from "./icons/Twitter.svelte"
+  import Discord from "./icons/Discord.svelte"
+
+  export let title:string, url:string
+
+  const getIconComponent = (url:string) => {
+    switch(true) {
+      case url.includes('twitter'):
+        return Twitter
+        
+      case url.includes('github'):
+        return Github
+
+      case url.includes('linkedin'):
+        return Linkedin
+
+      case url.includes('discord'):
+        return Discord
+
+      default:
+        return 
+    }
+  }
+</script>
+
+<a
+  aria-label={`${title} link`}
+  href={url}
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <svelte:component this={getIconComponent(url)} />
+</a>
